1de828296eee6ce8a1b4080cf9be562e9ff2fbe9
samybakk <samy.bakkouche@student.umons.ac.be>
2019-05-15 22:38:02 +0200

    coucou

--- app/src/main/java/com/example/ei_appm/MainActivity.java
+++ app/src/main/java/com/example/ei_appm/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.ei_appm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,7 +18,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView list = (ListView) findViewById(R.id.taskList);
+        Button bajout = (Button) findViewById(R.id.bajout);
+
+        bajout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ajout_acti = new Intent(MainActivity.this, ajout.class);
+                startActivity(ajout_acti);
+            }
+        });
+
+     /*   ListView list = (ListView) findViewById(R.id.taskList);
 
         Task task1 = new Task(42,"Exemple de tâche", 3,new Date(2019,5,15));
 
@@ -26,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<Task> taskList = taskBDD.getAllTasks();
         taskBDD.close();
         ArrayAdapter<Task> adapter = new ArrayAdapter<Task>(this, android.R.layout.simple_list_item_1, taskList);
-        list.setAdapter(adapter);
+        list.setAdapter(adapter);*/
 
     }
 }
