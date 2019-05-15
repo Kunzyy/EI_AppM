2ad10d2cb46e9b10a9067396557487b37fc6c509
Kunzyy <44604068+Kunzyy@users.noreply.github.com>
2019-05-15 19:02:41 +0200

    Kuntz

--- app/src/main/java/com/example/ei_appm/TaskDataBase.java
+++ app/src/main/java/com/example/ei_appm/TaskDataBase.java
@@ -4,16 +4,16 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class TasksDataBase extends SQLiteOpenHelper {
+public class TaskDataBase extends SQLiteOpenHelper {
 
-        private static final String TABLE_LIVRES = "table_tasks";
+        private static final String TABLE_TASKS = "table_tasks";
         private static final String COL_ID = "ID";
         private static final String COL_NAME = "NAME";
 
-        private static final String CREATE_BDD = "CREATE TABLE " + TABLE_LIVRES + " (" +
+        private static final String CREATE_BDD = "CREATE TABLE " + TABLE_TASKS + " (" +
                 COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_NAME + " TEXT NOT NULL);";
 
-        public TasksDataBase(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        public TaskDataBase(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
             super (context, name, factory, version);
         }
 
@@ -24,7 +24,7 @@ public class TasksDataBase extends SQLiteOpenHelper {
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("DROP TABLE " + TABLE_LIVRES);
+            db.execSQL("DROP TABLE " + TABLE_TASKS);
             onCreate(db);
         }
 }
