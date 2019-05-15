2bc3a663fddddcf80f0dc88fd3c3a2e23e364b6a
Kunzyy <44604068+Kunzyy@users.noreply.github.com>
2019-05-15 18:51:54 +0200

    Kuntz

---
+++ app/src/main/java/com/example/ei_appm/Task.java
@@ -0,0 +1,38 @@
+package com.example.ei_appm;
+
+public class Task {
+    private int id;
+    private String name;
+    private String description;
+
+    public Task() {}
+
+    public Task(String name, String descr) {
+        this.name = name;
+        this.description = descr;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Nom du livre = " + name + "\n" + "Description du livre = " + description);
+        return sb.toString();
+    }
+
+}
