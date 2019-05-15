cfebedffd39acd7b254c7fc7e9ce78db23c72579
Kunzyy <44604068+Kunzyy@users.noreply.github.com>
2019-05-15 21:32:02 +0200

    Kuntz

--- app/src/main/java/com/example/ei_appm/affiche.java
+++ app/src/main/java/com/example/ei_appm/affiche.java
@@ -3,11 +3,11 @@ package com.example.ei_appm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class regarder extends AppCompatActivity {
+public class affiche extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_regarder);
+        setContentView(R.layout.activity_affiche);
     }
 }
