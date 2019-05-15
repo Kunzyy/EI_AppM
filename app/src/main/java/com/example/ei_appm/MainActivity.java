971bc9d257cc90f1e7ce202f61c50281537db4e3
Kunzyy <44604068+Kunzyy@users.noreply.github.com>
2019-05-15 19:37:36 +0200

    Kuntz

--- app/src/main/java/com/example/ei_appm/MainActivity.java
+++ app/src/main/java/com/example/ei_appm/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.ei_appm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView list = (ListView) findViewById(R.id.taskList);
+
+        Task task1 = new Task("Exemple de tâche", 3,new Date(2019,5,15));
+
+        TaskBDD livreBdd = new TaskBDD(this);
+        livreBdd.openForWrite();
+        livreBdd.insertTask(task1);
+
+        ArrayList<Task> livreList = livreBdd.getAllTasks();
+        livreBdd.close();
+        ArrayAdapter<Task> adapter = new ArrayAdapter<Task>(this, android.R.layout.simple_list_item_1, livreList);
+        list.setAdapter(adapter);
+
     }
 }
