861ffcb645d7c4c873219008da1daaff517330d9
samybakk <samy.bakkouche@student.umons.ac.be>
2019-05-15 21:07:06 +0200

    coucou

--- app/src/main/java/com/example/ei_appm/TaskSQLite.java
+++ app/src/main/java/com/example/ei_appm/TaskSQLite.java
@@ -9,10 +9,12 @@ public class TaskSQLite extends SQLiteOpenHelper {
         private static final String TABLE_TASKS = "table_tasks";
         private static final String COL_ID = "ID";
         private static final String COL_NAME = "NAME";
+        private static final String COL_DUREE = "DUREE";
+        private static final String COL_DATE = "DATE";
 
 
         private static final String CREATE_BDD = "CREATE TABLE " + TABLE_TASKS + " (" +
-                COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_NAME + " TEXT NOT NULL);";
+                COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_NAME + " TEXT NOT NULL," + COL_DUREE + " INTEGER NOT NULL," + COL_DATE +" DATE"+");";
 
         public TaskSQLite(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
             super (context, name, factory, version);
